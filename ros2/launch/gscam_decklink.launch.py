3c528da5ad7d4560c16881d3abd7d66e22d38cfe
Anton Deguet <anton.deguet@jhu.edu>
2024-06-13 18:44:20 -0400

    working gscam_decklink_stereo + goovis for ROS2, renaming some ROS1 launch files and parameters to match ROS2, ROS2 still missing processing if calibrated

--- ros2/launch/gscam_decklink.launch.py
+++ ros2/launch/gscam_decklink.launch.py
@@ -25,6 +25,36 @@ def generate_launch_description():
         description = 'deckink connection to use',
         default_value = 'sdi')
 
+    images_per_second_configuration = LaunchConfiguration('images_per_second')
+    images_per_second_argument = DeclareLaunchArgument(
+        'images_per_second',
+        description = 'add rate filter to the gstreamer pipeline',
+        default_value = '0')
+
+    crop_top_configuration = LaunchConfiguration('crop_top')
+    crop_top_argument = DeclareLaunchArgument(
+        'crop_top',
+        description = 'pixels to crop from top of image',
+        default_value = '0')
+
+    crop_bottom_configuration = LaunchConfiguration('crop_bottom')
+    crop_bottom_argument = DeclareLaunchArgument(
+        'crop_bottom',
+        description = 'pixels to crop from bottom of image',
+        default_value = '0')
+
+    crop_left_configuration = LaunchConfiguration('crop_left')
+    crop_left_argument = DeclareLaunchArgument(
+        'crop_left',
+        description = 'pixels to crop from left of image',
+        default_value = '0')
+
+    crop_right_configuration = LaunchConfiguration('crop_right')
+    crop_right_argument = DeclareLaunchArgument(
+        'crop_right',
+        description = 'pixels to crop from right of image',
+        default_value = '0')
+
     deinterlace_configuration = LaunchConfiguration('deinterlace')
     deinterlace_argument = DeclareLaunchArgument(
         'deinterlace',
@@ -49,13 +79,24 @@ def generate_launch_description():
                 '"decklinkvideosrc"',
                 ' + " device-number=" + "', device_configuration, '"',
                 ' + " connection=" + "', connection_configuration, '"',
+                
+                ' + (" ! videorate ! video/x-raw,framerate=', images_per_second_configuration, '/1" if ', images_per_second_configuration, ' != 0 else "")',
+
+                ' + (" ! videocrop top=', crop_top_configuration, ' bottom=', crop_bottom_configuration,
+                ' left=', crop_left_configuration, ' right=', crop_right_configuration,
+                '" if ((', crop_top_configuration, ' > 0) or (', crop_bottom_configuration, ' > 0) or (',
+                crop_left_configuration, ' > 0) or (', crop_right_configuration, ' > 0)) else "")',
+                
                 ' + (" ! deinterlace" if ', deinterlace_configuration, ' else "")',
+                
                 ' + (" ! tee name=t t. ! queue ! glimagesink force-aspect-ratio=false t. ! queue" if ', glimagesink_configuration, ' else "")',
+                
                 ' + " ! videoconvert"'
             ]),
             'camera_info_url': camera_info_url_configuration,
             'camera_name': camera_name_configuration,
             'frame_id': PythonExpression(['"/', camera_name_configuration, '" + "_frame"']),
+            'synk_sink': 'false',
         }],
         remappings=[
             ('camera/camera_info', 'camera_info'),
@@ -67,6 +108,11 @@ def generate_launch_description():
         camera_name_argument,
         device_argument,
         connection_argument,
+        images_per_second_argument,
+        crop_top_argument,
+        crop_bottom_argument,
+        crop_left_argument,
+        crop_right_argument,
         deinterlace_argument,
         glimagesink_argument,
         gscam_node
--- ros2/launch/gscam_decklink_stereo.launch.py
+++ ros2/launch/gscam_decklink_stereo.launch.py
@@ -9,26 +9,56 @@ def generate_launch_description():
     stereo_rig_name_configuration = LaunchConfiguration('stereo_rig_name')
     stereo_rig_name_argument = DeclareLaunchArgument('stereo_rig_name',
                                                      description = 'stereo right_camera_name')
-    
+
     left_device_configuration = LaunchConfiguration('left_device')
     left_device_argument = DeclareLaunchArgument(
         'left_device',
         description = 'decklink device to use for left video',
         default_value = '1')
-    
+
     right_device_configuration = LaunchConfiguration('right_device')
     right_device_argument = DeclareLaunchArgument(
         'right_device',
         description = 'decklink device to use for right video',
         default_value = '0')
-    
+
+    images_per_second_configuration = LaunchConfiguration('images_per_second')
+    images_per_second_argument = DeclareLaunchArgument(
+        'images_per_second',
+        description = 'add rate filter to the gstreamer pipeline',
+        default_value = '0')
+
+    crop_top_configuration = LaunchConfiguration('crop_top')
+    crop_top_argument = DeclareLaunchArgument(
+        'crop_top',
+        description = 'pixels to crop from top of image',
+        default_value = '0')
+
+    crop_bottom_configuration = LaunchConfiguration('crop_bottom')
+    crop_bottom_argument = DeclareLaunchArgument(
+        'crop_bottom',
+        description = 'pixels to crop from bottom of image',
+        default_value = '0')
+
+    crop_left_configuration = LaunchConfiguration('crop_left')
+    crop_left_argument = DeclareLaunchArgument(
+        'crop_left',
+        description = 'pixels to crop from left of image',
+        default_value = '0')
+
+    crop_right_configuration = LaunchConfiguration('crop_right')
+    crop_right_argument = DeclareLaunchArgument(
+        'crop_right',
+        description = 'pixels to crop from right of image',
+        default_value = '0')
+
     deinterlace_configuration = LaunchConfiguration('deinterlace')
     deinterlace_argument = DeclareLaunchArgument(
         'deinterlace',
         description = 'add deinterlace filter to the gstreamer pipeline',
         default_value = 'True',
         choices = ['True', 'False', '0', '1'])
-    
+
     glimagesink_configuration = LaunchConfiguration('glimagesink')
     glimagesink_argument = DeclareLaunchArgument(
         'glimagesink',
@@ -43,16 +73,41 @@ def generate_launch_description():
 
     left_gscam_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(gscam_launch),
-        launch_arguments = {'camera_name': 'left', 'device': '1'}.items())
-    
+        launch_arguments =
+        {'camera_name': 'left',
+         'device': left_device_configuration,
+         'images_per_second': images_per_second_configuration,
+         'crop_top': crop_top_configuration,
+         'crop_bottom': crop_bottom_configuration,
+         'crop_left': crop_left_configuration,
+         'crop_right': crop_right_configuration,
+         'deinterlace': deinterlace_configuration,
+         'glimagesink': glimagesink_configuration,
+        }.items())
+
     right_gscam_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(gscam_launch),
-        launch_arguments = {'camera_name': 'right', 'device': '2'}.items())
-    
+        launch_arguments =
+        {'camera_name': 'right',
+         'device': right_device_configuration,
+         'images_per_second': images_per_second_configuration,
+         'crop_top': crop_top_configuration,
+         'crop_bottom': crop_bottom_configuration,
+         'crop_left': crop_left_configuration,
+         'crop_right': crop_right_configuration,
+         'deinterlace': deinterlace_configuration,
+         'glimagesink': glimagesink_configuration,
+        }.items())
+
     return LaunchDescription([
         stereo_rig_name_argument,
         left_device_argument,
         right_device_argument,
+        images_per_second_argument,
+        crop_top_argument,
+        crop_bottom_argument,
+        crop_left_argument,
+        crop_right_argument,
         deinterlace_argument,
         glimagesink_argument,
         left_gscam_node,
